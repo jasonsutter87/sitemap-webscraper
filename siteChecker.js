4ada1bf44330ad7ccf2b1d65e36d9d25fe0ec4e6
JasonS <jason@infostarproductions.com>
2019-05-21 15:23:44 -0700

    Parsing through text on every page

--- siteChecker.js
+++ siteChecker.js
@@ -2,6 +2,19 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'http://wolfriver.infostarproductions.com/sitemap_page/';
 
+
+function parseSite(extention){
+  rp('http://wolfriver.infostarproductions.com' + extention)
+  .then(function(html){
+    //success!
+    console.log($('p', html).text());
+  })
+  .catch(function(err){
+    //handle error
+  });
+}
+
+
 rp(url)
   .then(function(html){
     //success!
@@ -9,6 +22,8 @@ rp(url)
     var siteURlsSize = ($('.site_map_category > li > a', html).length );
     for(var i = 0; i < siteURlsSize; i++){
       console.log(siteURls[i].attribs.href);
+
+      parseSite(siteURls[i].attribs.href)
     }
   })
   .catch(function(err){

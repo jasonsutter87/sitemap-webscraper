0ed116cf698aea8a9bdd614cd566780bdbbaaa72
JasonS <jason@infostarproductions.com>
2019-05-21 15:02:48 -0700

    parsing through site map to get all the links on the page

--- siteChecker.js
+++ siteChecker.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const rp = require('request-promise');
+const $ = require('cheerio');
+const url = 'http://wolfriver.infostarproductions.com/sitemap_page/';
+
+rp(url)
+  .then(function(html){
+    //success!
+    var siteURls = $('.site_map_category > li > a', html);
+    var siteURlsSize = ($('.site_map_category > li > a', html).length );
+    for(var i = 0; i < siteURlsSize; i++){
+      console.log(siteURls[i].attribs.href);
+    }
+  })
+  .catch(function(err){
+    //handle error
+  });
